fcee1721d5d7725618af501204da187e816d6a63
turiski <turiskir@post.bgu.ac.il>
2023-05-31 14:42:23 +0300

    User - GET.POST MY.FAVORITE.FAMILY-RECIPES

--- routes/user.js
+++ routes/user.js
@@ -25,9 +25,12 @@ router.use(async function (req, res, next) {
  * This path gets body with recipeId and save this recipe in the favorites list of the logged-in user
  */
 router.post('/favorites', async (req,res,next) => {
+  print ("in favorites");
   try{
     const user_id = req.session.user_id;
+    print (user_id);
     const recipe_id = req.body.recipeId;
+    print (recipe_id);
     await user_utils.markAsFavorite(user_id,recipe_id);
     res.status(200).send("The Recipe successfully saved as favorite");
     } catch(error){
@@ -49,6 +52,64 @@ router.get('/favorites', async (req,res,next) => {
     res.status(200).send(results);
   } catch(error){
     next(error); 
+  }
+});
+
+router.get('/MyRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    let favorite_recipes = {};
+    const recipes_id = await user_utils.getMyRecipes(user_id);
+    let recipes_id_array = [];
+    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  } catch(error){
+    next(error); 
+  }
+});
+
+
+router.get('/MyFamilyRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    let favorite_recipes = {};
+    const recipes_id = await user_utils.getMyFamilyRecipes(user_id);
+    let recipes_id_array = [];
+    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  }
+  catch(error){
+    next(error);
+  }
+});
+
+router.post('/MyFamilyRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipe_name = req.body.recipeName;
+    const recipe_category = req.body.recipeCategory;
+    const recipe_ingredients = req.body.recipeIngredients;
+    const recipe_instructions = req.body.recipeInstructions;
+    const recipe_image = req.body.recipeImage;
+    await user_utils.addNewFamilyRecipe(user_id, recipe_name, recipe_category, recipe_ingredients, recipe_instructions, recipe_image);
+    res.status(200).send("The Recipe successfully saved as favorite");
+  }
+  catch(error){
+    next(error);
+  }
+});
+
+router.post('/MyRecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    await user_utils.addNewRecipe(user_id, recipe_id);
+    res.status(200).send("The Recipe successfully saved as favorite");
+  }
+  catch(error){
+    next(error);
   }
 });
 
--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -10,6 +10,38 @@ async function getFavoriteRecipes(user_id){
 }
 
 
-
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
+
+
+
+//  ADDING A NEW RECIPE
+
+async function addNewRecipe(user_id, recipe_name, recipe_category, recipe_ingredients, recipe_instructions, recipe_image){
+    await DButils.execQuery(`insert into Recipes values ('${user_id}', '${recipe_name}', '${recipe_category}', '${recipe_ingredients}', '${recipe_instructions}', '${recipe_image}')`);
+}
+exports.addNewRecipe = addNewRecipe;
+
+// GETTING MY RECIPES
+
+async function getMyRecipes(user_id){
+    const recipes_id = await DButils.execQuery(`select recipe_id from Recipes where user_id='${user_id}'`);
+    return recipes_id;
+}
+exports.getMyRecipes = getMyRecipes;
+
+// GETTING MY FAMILY RECIPES
+
+async function getMyFamilyRecipes(user_id){
+    const recipes_id = await DButils.execQuery(`select recipe_id from FamilyRecipes where user_id='${user_id}'`);
+    return recipes_id;
+}
+exports.getMyFamilyRecipes = getMyFamilyRecipes;
+
+// ADDING A NEW FAMILY RECIPE
+
+async function addNewFamilyRecipe(user_id, recipe_name, recipe_category, recipe_ingredients, recipe_instructions, recipe_image){
+    await DButils.execQuery(`insert into FamilyRecipes values ('${user_id}', '${recipe_name}', '${recipe_category}', '${recipe_ingredients}', '${recipe_instructions}', '${recipe_image}')`);
+}
+exports.addNewFamilyRecipe = addNewFamilyRecipe;
+
