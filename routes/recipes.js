bd947a0e499c616d81b2439cafadbef773a2f385
turiski <turiskir@post.bgu.ac.il>
2023-06-03 20:41:41 +0300

    FinishCheckingUser-My,Favorite,Family recipesDone

--- routes/recipes.js
+++ routes/recipes.js
@@ -7,42 +7,42 @@ router.get("/", (req, res) => res.send("im here"));
 
 
 
-router.post("/", async (req, res, next) => {
-  try {
-    // parameters exists
-    // valid parameters
-    // username exists
-    let user_details = {
-      id: req.body.id,
-      title: req.body.title,
-      image: req.body.image,
-      readyInMinutes: req.body.readyInMinutes,
-      popularity: req.body.popularity,
-      vegetarian: req.body.vegetarian,
-      vegan: req.body.vegan,
-      glutenFree: req.body.glutenFree
-    }
-    let users = [];
-    users = await DButils.execQuery("SELECT title from recipes");
+// router.post("/", async (req, res, next) => {
+//   try {
+//     // parameters exists
+//     // valid parameters
+//     // username exists
+//     let user_details = {
+//       id: req.body.id,
+//       title: req.body.title,
+//       image: req.body.image,
+//       readyInMinutes: req.body.readyInMinutes,
+//       popularity: req.body.popularity,
+//       vegetarian: req.body.vegetarian,
+//       vegan: req.body.vegan,
+//       glutenFree: req.body.glutenFree
+//     }
+//     let users = [];
+//     users = await DButils.execQuery("SELECT title from recipes");
 
-    if (users.find((x) => x.username === user_details.username))
-      throw { status: 409, message: "recipe allready written before" };
+//     if (users.find((x) => x.username === user_details.username))
+//       throw { status: 409, message: "recipe allready written before" };
 
-    // add the new username
-    // let hash_password = bcrypt.hashSync(
-    //   user_details.password,
-    //   parseInt(process.env.bcrypt_saltRounds)
-    // );
-    await DButils.execQuery(
-      `INSERT INTO recipes (title, image, readyInMinutes, popularity, vegetarian, vegan, glutenFree) 
-      VALUES ('${user_details.title}', '${user_details.image}', ${user_details.readyInMinutes}, 
-      ${user_details.popularity}, ${user_details.vegetarian}, ${user_details.vegan}, ${user_details.glutenFree})`
-    );
-    res.status(201).send({ message: "recipe created", success: true });
-  } catch (error) {
-    next(error);
-  }
-});
+//     // add the new username
+//     // let hash_password = bcrypt.hashSync(
+//     //   user_details.password,
+//     //   parseInt(process.env.bcrypt_saltRounds)
+//     // );
+//     await DButils.execQuery(
+//       `INSERT INTO recipes (title, image, readyInMinutes, popularity, vegetarian, vegan, glutenFree) 
+//       VALUES ('${user_details.title}', '${user_details.image}', ${user_details.readyInMinutes}, 
+//       ${user_details.popularity}, ${user_details.vegetarian}, ${user_details.vegan}, ${user_details.glutenFree})`
+//     );
+//     res.status(201).send({ message: "recipe created", success: true });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 
 
@@ -51,7 +51,13 @@ router.post("/", async (req, res, next) => {
  */
 router.get("/:recipeId", async (req, res, next) => {
   try {
-    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    console.log(req.params.recipeId);
+    recipe_id_array=[req.params.recipeId]
+    if (req.params.recipeId.length===0){
+      throw { status: 401, message: "missing recipe_id" };
+    }
+      
+    const recipe = await recipes_utils.getRecipeDetails(recipe_id_array);
     res.send(recipe);
   } catch (error) {
     next(error);
