dd97453cf31b28090035fefe8d18342e6a233d09
DuaniE <103958872+DuaniE@users.noreply.github.com>
2023-06-05 15:49:04 +0300

    Update user_utils.js

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,23 +1,14 @@
 
 const DButils = require("./DButils");
-const recipes_utils = require("./recipes_utils");
-
-
-
-// async function markAsFavorite(user_id, recipe_id){
-//     await DButils.execQuery(`insert into favoriterecipes values ('${user_id}',${recipe_id})`);
-// }
-
 async function markAsFavorite(user_id, recipe_id){
-    // check if the recipe is already in the favorite list of API
-       try{       
-        const recipe = await recipes_utils.getRecipeInformation(recipe_id);
-        await DButils.execQuery(`insert into favoriterecipes values ('${user_id}',${recipe_id})`);
-       }catch(error){
-        throw new Error('Failed to mark recipe as favorite: ' + error.message);
-      }
+     // check if the recipe is already in the favorite list
+        const recipe = await DButils.execQuery(`select recipe_id from favoriterecipes where user_id='${user_id}' and recipe_id in (SELECT recipe_id FROM recipes))`);
+        if (recipe.length != 0){
+            throw { status: 409, message: "recipe allready in favorite list" };
+        }
+    // add the new recipe
+    await DButils.execQuery(`insert into favoriterecipes values ('${user_id}',${recipe_id})`);
 }
-
 async function getFavoriteRecipes(user_id){
     const recipes_id = await DButils.execQuery(`select recipe_id from favoriterecipes where user_id='${user_id}'`);
     return recipes_id;
@@ -58,3 +49,58 @@ exports.getMyFamilyRecipes = getMyFamilyRecipes;
 //     await DButils.execQuery(`insert into FamilyRecipes values ('${user_id}', '${recipe_name}', '${recipe_category}', '${recipe_ingredients}', '${recipe_instructions}', '${recipe_image}')`);
 // }
 // exports.addNewFamilyRecipe = addNewFamilyRecipe;
+async function getLast3Watch(user_id){
+    const lastThree = await DButils.execQuery(`select H_W_R1,H_W_R1,H_W_R1 from watched where user_id='${user_id}'`);
+    return lastThree.map((row) => {
+        const {
+            History_Watch_R1,
+            History_Watch_R2,
+            History_Watch_R3
+        } = row;
+        return {
+            History_Watch_1: History_Watch_R1,
+            History_Watch_2: History_Watch_R2,
+            History_Watch_3: History_Watch_R3
+        }
+})
+}
+exports.getLast3Watch = getLast3Watch;
+
+//TODO: need to change names of variables and create a table named watched
+async function UpdateLast3Watched(user_id, recipe_id) {
+    const last_3_ans = await DButils.execQuery(`select * from user_last_3_watch where user_id='${user_id}'`);
+    if (last_3_ans.length === 0) {
+        const History_Watch_R1 = recipe_id;
+        const History_Watch_R2 = 0;
+        const History_Watch_R3 = 0;
+        await DButils.execQuery(`insert into user_last_3_watch (user_id,History_Watch_R1,History_Watch_R2,History_Watch_R3) VALUES ('${user_id}', '${History_Watch_R1}', '${History_Watch_R2}','${History_Watch_R3}')`);
+    }
+    else if (last_3_ans.length === 1)
+    {
+        const History_Watch_R1 = last_3_ans[0].History_Watch_R1;
+        const History_Watch_R2 = recipe_id;
+        const History_Watch_R3 = 0;
+        await DButils.execQuery(`insert into user_last_3_watch (user_id,History_Watch_R1,History_Watch_R2,History_Watch_R3) VALUES ('${user_id}', '${History_Watch_R1}', '${History_Watch_R2}','${History_Watch_R3}')`);
+    }
+    else if(last_3_ans.length === 2)
+    {
+        const History_Watch_R1 = last_3_ans[0].History_Watch_R1;
+        const History_Watch_R2 = last_3_ans[0].History_Watch_R2;
+        const History_Watch_R3 = recipe_id;
+        await DButils.execQuery(`insert into user_last_3_watch (user_id,History_Watch_R1,History_Watch_R2,History_Watch_R3) VALUES ('${user_id}', '${History_Watch_R1}', '${History_Watch_R2}','${History_Watch_R3}')`);
+    }
+    else {
+        //let results=Object.values(JSON.parse(JSON.stringify(DB_ans)));
+        let History_Watch_R1 = last_3_ans[0].History_Watch_R1;
+        let History_Watch_R2 = last_3_ans[0].History_Watch_R2;
+        let History_Watch_R3
+        History_Watch_R3 = History_Watch_R2;
+        History_Watch_R2 = History_Watch_R1;
+        History_Watch_R1 = recipe_id;
+        console.log(History_Watch_R1);
+        console.log(History_Watch_R2);
+        console.log(History_Watch_R3);
+        await DButils.execQuery(`update user_last_3_watch set History_Watch_R1='${History_Watch_R1}',History_Watch_R2='${History_Watch_R2}',History_Watch_R3='${History_Watch_R3}' where user_id='${user_id}'`);
+    }
+}
+exports.UpdateLast3Watched = UpdateLast3Watched;
